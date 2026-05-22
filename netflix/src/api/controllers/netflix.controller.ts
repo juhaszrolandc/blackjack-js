700fb8c33454c1a6274be7dc7aab1e161c6cfe95
Juhász Roland <h363075@stud.u-szeged.hu>
2026-05-22 17:13:22 +0200

    basic API

---
+++ netflix/src/api/controllers/netflix.controller.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from 'express';
+
+function createUser(req: Request, res: Response) {
+  res.status(201).json({
+    "message": "Account has been successfully created"
+  });
+}
+
+function login(req: Request, res: Response){
+  res.status(200).json({
+    "session-id": "123e4567-e89b-12d3-a456-426614174000"
+  });
+}
+
+function logout(req: Request, res: Response){
+  res.status(200).json({
+    "message": "Logout successfully"
+  });
+}
+
+function queue(req: Request, res: Response){
+  console.log("queue");
+  res.status(200).json([
+      {
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+      }
+  ]);
+}
+
+function findMovie(req: Request, res: Response){
+  res.status(200).json({
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+  });
+}
+
+function createMovie(req: Request, res: Response){
+  res.status(201).json({
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+  });
+}
+
+function deleteMovie(req: Request, res: Response){
+  res.status(204).json({
+    "message": "You deleted the movie successfully."
+  });
+}
+
+function updateMovie(req: Request, res: Response){
+  res.status(200);
+}
+
+function rentMovieById(req: Request, res: Response){
+  res.status(200).json({
+        "message": "You rented the movie successfully."
+  });
+}
+
+export {
+  createUser,
+  login,
+  logout,
+  queue,
+  findMovie,
+  createMovie,
+  deleteMovie,
+  updateMovie,
+  rentMovieById
+};
---
+++ netflix/src/tests/controllers/netfilx.controller.test.ts
@@ -0,0 +1,185 @@
+import { expect } from 'chai';
+import axios from 'axios';
+import '../../app';
+
+describe('Netfilx controller', function () {
+  const instance = axios.create({
+    baseURL: 'http://localhost:3000/v1',
+    validateStatus: undefined
+  });
+
+  describe('POST /user', function () {
+    it('should be available', async () => {
+      const response = await instance.post('/user', {
+        "username": "Roland",
+        "password": "123"
+      });
+      expect(response.status).to.equal(201);
+      expect(response.data).to.deep.equal({
+        "message": "Account has been successfully created"
+      });
+    });
+  });
+
+  describe('POST /user/login', function () {
+    it('should be available', async () => {
+      const body = {
+        "username": "Roland",
+        "password": "123"
+      }
+      const response = await instance.post('/user/login', body);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal({
+        "session-id": "123e4567-e89b-12d3-a456-426614174000"
+      });
+    });
+  });
+  
+  describe('POST /user/logout', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Session-ID": "123e4567-e89b-12d3-a456-426614174000",
+        }
+      };
+      const response = await instance.post('/user/logout', {}, config);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal({
+        "message": "Logout successfully"
+      });
+    });
+  });
+
+  describe('GET /queue', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Session-ID": "123e4567-e89b-12d3-a456-426614174000"
+        }
+      };
+      const response = await instance.get('/queue?order=false', config);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal([
+        {
+          "title": "Transformers",
+          "description": "Transformers (2007) is a high-octane science fiction action film.",
+          "directed-by": "Michael Bay",
+          "actors": ["Shia LaBeouf", "Megan Fox"],
+          "categories": ["sci-fi", "action"],
+          "type": "Movie",
+          "id": 3
+        }
+      ]);
+    });
+  });
+
+  describe('GET /movies', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Session-ID": "123e4567-e89b-12d3-a456-426614174000",
+      }};
+      const response = await instance.get('/movies?id=3', config);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal({
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+      });
+    });
+  });
+
+  describe('POST /movies', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Admin-API-key": "123e4567-e89b-12d3-a456-426614174000",
+        }
+      };
+      const movie = {
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie"
+      };
+      const response = await instance.post('/movies', movie, config);
+      expect(response.status).to.equal(201);
+      expect(response.data).to.deep.equal({
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+      });
+    });
+  });
+
+  describe('DELETE /movies', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Admin-API-key": "123e4567-e89b-12d3-a456-426614174000",
+        }
+      };
+      const movie = {
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+      };
+      const response = await instance.delete('/movies', config);
+      expect(response.status).to.equal(204);
+      expect(response.data).to.deep.equal({
+        "message": "You deleted the movie successfully."
+      });
+    });
+  });
+
+  describe('PATCH /movies', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Admin-API-key": "123e4567-e89b-12d3-a456-426614174000",
+        }
+      };
+      const response = await instance.patch('/movies?id=3', config);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal({
+        "title": "Transformers",
+        "description": "Transformers (2007) is a high-octane science fiction action film.",
+        "directed-by": "Michael Bay",
+        "actors": ["Shia LaBeouf", "Megan Fox"],
+        "categories": ["sci-fi", "action"],
+        "type": "Movie",
+        "id": 3
+      });
+    });
+  });
+
+  describe('GET /movies/{movieId}/rent', function () {
+    it('should be available', async () => {
+      const config = {
+        "headers": {
+          "X-Session-ID": "123e4567-e89b-12d3-a456-426614174000"
+        }
+      };
+      const response = await instance.patch('/movies/3/rent', config);
+      expect(response.status).to.equal(200);
+      expect(response.data).to.deep.equal({
+        "message": "You rented the movie successfully."
+      });
+    });
+  });
+
+});
